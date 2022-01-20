a6f3bfc078e03dc8cafb087ae6804959ec62fc05
Daniel <gersmand@gmail.com>
2022-01-20 08:37:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Person.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+public class Person {
+    String name;
+    int id;
+    String phoneNumber;
+
+
+
+    public Person(String name, int id,String phoneNumber) {
+        this.name = name;
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+
+
+}
