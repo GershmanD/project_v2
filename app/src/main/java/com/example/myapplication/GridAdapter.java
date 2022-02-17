0efa870b49d86de4842a92653912a0b4f4465adb
Daniel <gersmand@gmail.com>
2022-02-17 09:13:04 +0200

    i copies :)

--- app/src/main/java/com/example/myapplication/GridAdapter.java
+++ app/src/main/java/com/example/myapplication/GridAdapter.java
@@ -1,38 +1,40 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class GridAdapter extends BaseAdapter {
-    Context context;
-    String[] number_of_bus;
-    int image;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-    public GridAdapter(Context context, String[] number_of_bus, int image) {
-        this.context = context;
-        this.number_of_bus = number_of_bus;
-        this.image = image;
+import java.util.ArrayList;
+
+public class GridAdapter extends ArrayAdapter<Bus> {
+
+    public GridAdapter(@NonNull Context context, ArrayList<Bus> buses) {
+        super(context, 0, buses);
     }
 
+    @NonNull
     @Override
-    public int getCount() {
-        return number_of_bus.length;
-    }
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listitemView = convertView;
+        if (listitemView == null) {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.grid_element, parent, false);
+        }
 
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
+        Bus tmp_bus = getItem(position);
+        TextView tv_busnum_grid = listitemView.findViewById(R.id.tv_busnum_grid);
+        ImageView iv_bus_grid = listitemView.findViewById(R.id.iv_bus_grid);
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        tv_busnum_grid.setText("bus #"+String.valueOf(tmp_bus.getBus_number()));
+        iv_bus_grid.setImageResource(R.drawable.bus);
+        return listitemView;
     }
 }
